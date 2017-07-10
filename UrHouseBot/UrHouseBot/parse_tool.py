e3c8e7151fad94b2204d985422128d29bd6e71b8
takeTrace <taketrace@takeTraces-MacBook-Pro.local>
2017-07-10 22:06:23 +0800

    check to parse_group()

--- UrHouseBot/UrHouseBot/parse_tool.py
+++ UrHouseBot/UrHouseBot/parse_tool.py
@@ -3,16 +3,50 @@ import re
 import random
 from datetime import datetime
 
+keyword = [
+    '徐家汇',
+    '湖南路',
+    '上海图书馆',
+    '武康路',
+    '乌鲁木齐',
+    '兴国路',
+    '幸福路',
+    '平武路',
+    '镇宁路',
+    '番禺路',
+    '愚园路',
+    '江苏路',
+]
 
-def get_next_page_link(self, response):
+def appendRoad():
+    keys =  keyword[0]
+    for key in keyword[1:]:
+        keys = keys + "|" + key
+    return keys
+
+def get_next_page_link(response):
     next = response.css('.next a::attr(href)').extract_first()
     print('下一页: ' + next)
     return next
 
-def is_need_parse(self, response):
-    date = response.css('tbody .time').extract_first()
-    delta_time = datetime.now() - datetime.strptime(date, '%m-%d %h:%s')
+def is_need_parse(date):
+    if not re.search('.*\d{4}.*', date):
+        date = str(datetime.now().year) + '-' + date
+        dformat = format_date
+    if re.search(".*:.*", date):
+        dformat = format_dateTime
+    delta_time = datetime.now() - datetime.strptime(date, dformat)
     return delta_time.days < 30
 
-def random_cat(self):
-    return random.randint(1, 50) + 1000
+def random_cat():
+    return str(random.randint(1, 50) + 1000)
+
+def filter_title(title):
+    restr = ".*{0}+.*".format(appendRoad())
+    if re.search(restr, title):
+        return True
+    return False
+
+
+format_dateTime = '%Y-%m-%d %H:%M'
+format_date = '%Y-%m-%d'
--- UrHouseBot/UrHouseBot/spiders/doubanGroup.py
+++ UrHouseBot/UrHouseBot/spiders/doubanGroup.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-from UrHouseBot.UrHouseBot import parse_tool
+import UrHouseBot
+from UrHouseBot import parse_tool
 
 class DoubangroupSpider(scrapy.Spider):
-    key = '湖南路'
+    keys = parse_tool.keyword
     name = "doubanGroup"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
@@ -16,43 +16,90 @@ class DoubangroupSpider(scrapy.Spider):
         urls = ['https://www.douban.com/group/search?cat=1019&q=上海+租房/']
         for url in urls:
             yield scrapy.Request(url, headers=self.headers)
+
+
+
     def parse(self, response):
         # 解析搜索小组出来的租房的小组
-        print('-'*300)
+        print('-'*150)
         groups = response.css('.groups .result .title a')
         for group in groups:
-            t = group.css('::text').extract_first()
-            l = group.css('a::attr(href)').extract_first() + 'discussion?start=0'
+            t = group.css('::text').extract_first().strip()
+            l = group.css('a::attr(href)').extract_first().strip() + 'discussion?start=0'
             # gid = l.re(r'.*/(\d+)/.*')
             gid = group.css('a::attr(onclick)').re('.*sid: (\d+)}.*').pop()
             print('grpup id: ' + gid)
             print('link: ' + l)
             print('title: ' + t)
 
-            target_search_link = 'https://www.douban.com/group/search?group=' + gid + '&cat='+ self.random_cat() +'&q=' + self.key + '&sort=time'
+
+            # 1. 返回在某小组里根据关键词搜索的结果(直接拼接id和关键词即使搜索结果的url)
+            for key in self.keys:
+                target_search_link = 'https://www.douban.com/group/search?group=' + gid \
+                                     + '&cat=' + '1013' + '&q=' + key + '&sort=time'
+                yield scrapy.Request(target_search_link, headers=self.headers, callback=self.parse_search_result)
+
+
+
+            # 2. 返回解析某个小组的帖子的列表, 可以翻页
             yield scrapy.Request(l, headers=self.headers, callback=self.parse_group)
 
-        # 下一页
-        next = self.get_next_page_link(response)
+        # 3. 下一页
+        next = parse_tool.get_next_page_link(response)
         print('下一页: ' + next)
-        # yield scrapy.Request(next, headers=self.headers)
+        yield scrapy.Request(next, headers=self.headers)
         pass
+
+
 
     def parse_group(self, response):
         # 深度解析小组里的帖子
-        print('解析小组页面' + '^'*100)
+        gtl = response.css('head title::text').extract_first().strip()
+        print('解析' + gtl + ' 页面' + '^'*100)
+
+        # 获取下一页
+        next = parse_tool.get_next_page_link(response)
+        if next:
+            print(gtl + "小组的下一页" + next)
+            yield scrapy.Request(next, headers=self.headers, callback=self.parse_group)
+
+        titles = response.css('.olt .title')
+        for t in titles:
+            time = t.xpath('string(..//td[@class="time"][1])').extract_first().strip()
+            if parse_tool.is_need_parse(time) is False:
+                continue
+
+            # 时间符合要求
+            title = t.css('a::attr(title)::text').extract_first().strip()
+            print("需要解析: " + title)
+            link = t.css('a::attr(href)').extract_first()
+            # 默认解析详情
+            to_parse = self.parse_detail_article
+            if parse_tool.filter_title(title):
+                # title中含有关键字的话就直接解析目的文章
+                to_parse = self.parse_target_article
+            yield scrapy.Request(link, headers=self.headers, callback=to_parse)
 
         pass
+
+
+
 
     def parse_search_result(self, response):
-        # 解析根据设定的关键词的搜索结果的帖子
-        print('接续搜索的关键词: ' + self.key)
+        # 解析根据设定的关键词的搜索某小组的结果的帖子
+        print('接续搜索的关键词: ' + response.css('title::text')).re('.*:(.+)$').extract_first().strip()
 
         pass
+
+
+
 
     def parse_detail_article(self, response):
-        # 解析具体的帖子
-        print('解析具体梯子')
+        # 解析具体的帖子, 查看是否含有需要解析的关键要素
+        print('解析具体帖子' + response.url)
 
         pass
-    
+
+    def parse_target_article(self, response):
+        # 解析含有关键词的帖子,
+        print('帖子中包含有相关的关键词, 且未出现已出租的字样, 再次深度解析' + response.url)
